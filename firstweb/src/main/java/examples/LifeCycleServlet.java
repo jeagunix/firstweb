e9b9c285bbe16771ba66e36ae04b73040e00b81f
dlwpr <dlwpr@DESKTOP-R61EI7F>
2022-09-18 20:42:43 +0900

    feat: LifeCycleServle 실습2
    doPost, doGet overriding을 통해 get, post 메소드 실습

--- firstweb/src/main/java/examples/LifeCycleServlet.java
+++ firstweb/src/main/java/examples/LifeCycleServlet.java
@@ -1,6 +1,9 @@
 package examples;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -36,8 +39,41 @@ public class LifeCycleServlet extends HttpServlet {
 		System.out.println("destroy 호출!");
 	}
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("service 호출!"); //url이 호출될 때 마다 호출됨
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html>");
+		out.println("<head><title>form</title></head>");
+		out.println("<body>");
+		out.println("<form method='post' action='/firstweb/LifeCycleServlet'>");
+		out.println("name : <input type='text' name='name'><br>");
+		out.println("<input type='submit' value='ok'><br>");                                                 
+		out.println("</form>");
+		out.println("</body>");
+		out.println("</html>");
+		
+		out.close();
+	
 	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		
+		PrintWriter out = resp.getWriter();
+		String name = req.getParameter("name");
+		
+		out.println("<h1> hello " + name + "</h1>");
+		
+		out.close();
+		
+	}
+
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		System.out.println("service 호출!"); //url이 호출될 때 마다 호출됨
+//	}
 
 }
